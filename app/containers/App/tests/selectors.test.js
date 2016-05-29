5955eeefac2473e83cebcc56eba3fd161f096f6f
Brandon Braun <brandon.braun@gmail.com>
2016-05-29 12:35:06 -0400

    add App tests

--- app/containers/App/tests/selectors.test.js
+++ app/containers/App/tests/selectors.test.js
@@ -1,7 +1,34 @@
 import { fromJS } from 'immutable';
 import expect from 'expect';
 
-import { selectLocationState } from 'containers/App/selectors';
+import { selectLocationState, selectGlobal, selectDrawerOpen, selectSearchOpen } from 'containers/App/selectors';
+
+describe('selectGlobal', () => {
+  const globalSelector = selectGlobal();
+  it('should select the global state', () => {
+    const globalState = fromJS({});
+    const mockedState = fromJS({ global: globalState });
+    expect(globalSelector(mockedState)).toEqual(globalState);
+  });
+});
+
+describe('selectDrawerOpen', () => {
+  const drawerOpenSelector = selectDrawerOpen();
+  it('should select the drawer open status', () => {
+    const drawerState = true;
+    const mockedState = fromJS({ global: { drawerOpen: drawerState } });
+    expect(drawerOpenSelector(mockedState)).toEqual(drawerState);
+  });
+});
+
+describe('selectSearchOpen', () => {
+  const searchOpenSelector = selectSearchOpen();
+  it('should select the search open status', () => {
+    const searchState = true;
+    const mockedState = fromJS({ global: { searchOpen: searchState } });
+    expect(searchOpenSelector(mockedState)).toEqual(searchState);
+  });
+});
 
 describe('selectLocationState', () => {
   it('should select the route as a plain JS object', () => {
